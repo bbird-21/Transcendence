4172b1800f8928fb322aaa5b1ce4e3fa8cbbc038
mmeguedm <m.bbird21@proton.me>
2024-09-11 23:29:47 +0200

    [LOG-69]  Profile Page (#8)
    * front : add sidenav (home-page.html)
    
    * create sidenav.html && profile page in progress

--- django/ft_transcendence/core/static/core/js/navbar.js
+++ django/ft_transcendence/core/static/core/js/navbar.js
@@ -5,3 +5,15 @@ function openNav() {
 function closeNav() {
 	document.getElementById("SidenavID").style.width = "0";
 }
+
+function hideMenu() {
+    var burgerMenu = document.getElementById('burger-menu');
+    burgerMenu.classList.add('hidden');
+}
+
+function openMenu() {
+    setTimeout(function() {
+        var burgerMenu = document.getElementById('burger-menu');
+        burgerMenu.classList.remove('hidden');
+    }, 300); // Delay in milliseconds (2000ms = 2 seconds)
+}
