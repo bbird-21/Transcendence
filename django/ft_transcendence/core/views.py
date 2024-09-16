7f28100aa8d6f567f8daf904393bd181de51ccae
mmeguedm <m.bbird21@proton.me>
2024-09-16 14:39:56 +0200

    [Log 75]  : Extend User (upload avatar && fix forms) (#9)
    * Specify Login URL on @login_required && fixing forms on login.html
    
    * scope : <login.html> Update View To Pass Multiple Forms
    
    * fix(login.html): Makes the forms disappear if another form has been display

--- django/ft_transcendence/core/views.py
+++ django/ft_transcendence/core/views.py
@@ -9,28 +9,48 @@ from django.shortcuts import render
 from .forms import NameForm
 from .forms import SignupForm
 from .forms import SigninForm
+from .forms import UserProfileForm
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # --------- <login.html> ---------
 def login(request):
     if request.method == "POST":
-        form = SigninForm(request.POST)
-        if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+        signin_form = SigninForm(request.POST, prefix="signin")
+        signup_form = SignupForm(request.POST, prefix="signup")
+        if signin_form.is_valid():
+            user = authenticate(username=signin_form.cleaned_data['username'], password=signin_form.cleaned_data['password'])
             if user:
                 django_login(request, user)
                 return HttpResponseRedirect("/home")
+        elif signup_form.is_valid():
+            username = signup_form.cleaned_data["username"]
+            password = signup_form.cleaned_data["password"]
+            # process the data in signup_form.cleaned_data as required
+            signup_form.save()
+            user = User.objects.get(username=username)
+            user.set_password(password)
+            user = authenticate(username=username, password=password)
+            # A backend authenticated the credentials
+            if user is not None:
+                django_login(request, user)
+                return HttpResponseRedirect("/home/")
+                # username = signup_form.cleaned_data["username"]
+                # password = signup_form.cleaned_data["password"]
     else:
-        form = SigninForm()
-    return render(request, "core/login.html", {"form": form})
+        signin_form = SigninForm(prefix="signin")
+        signup_form = SignupForm(prefix="signup")
+    return render(request, "core/login.html", {
+        "signin_form": signin_form,
+        "signup_form": signup_form,
+    })
 
 def logout(request):
         django_logout(request)
         return redirect('login')  # If user is not authenticated, redirect to home
 
 def signup(request):
-    print("SIGNUP")
     # if this is a POST request we need to process the form data
     if request.method == "POST":
         # create a form instance and populate it with data from the request:
@@ -61,12 +81,31 @@ def signup(request):
 
 
 # --------- <home.html> ---------
+@login_required
 def home(request):
     user = request.user
     return render(request, "core/home.html", {"user": user})
 
+@login_required
 def profile(request):
-    return render(request, "core/profile.html")
+    if request.method == "POST":
+        form = UserProfileForm(request.POST)
+        if form.is_valid:
+            return HttpResponseRedirect("/home/")
+    else:
+        form = UserProfileForm()
+    return render(request, "core/profile.html", {"form": form})
+
 def test(request):
     return render(request, "core/test.html")
+
+@login_required
+def avatar(request):
+    if request.method == "POST":
+        form = UserProfileForm(request.POST)
+        if form.is_valid:
+            return HttpResponseRedirect("/home/")
+    else:
+        form = UserProfileForm()
+    return render(request, "core/profile.html", {"form": form})
 
