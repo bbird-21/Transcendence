41b6be3799a837cbb162dc55642af62a9e62d7dc
MEGUEDMINI Mohamed <m.bbird21@proton.me>
2024-09-16 14:44:32 +0200

    clean code

--- django/ft_transcendence/core/urls.py
+++ django/ft_transcendence/core/urls.py
@@ -3,11 +3,8 @@ from . import views
 
 urlpatterns = [
 	path("", views.login, name="login"),
-	path("home/", views.home, name="home"),
 	path("logout/", views.logout, name="logout"),
-	path("signup/", views.signup, name="signup"),
+	path("home/", views.home, name="home"),
 	path("profile/", views.profile, name="profile"),
-	path("avatar/", views.avatar, name="avatar"),
-	# path("form/", views.home, name="loginform"),
 	path("test/", views.test, name="test"),
 ]
--- django/ft_transcendence/core/views.py
+++ django/ft_transcendence/core/views.py
@@ -50,36 +50,6 @@ def logout(request):
         django_logout(request)
         return redirect('login')  # If user is not authenticated, redirect to home
 
-def signup(request):
-    # if this is a POST request we need to process the form data
-    if request.method == "POST":
-        # create a form instance and populate it with data from the request:
-        form = SignupForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
-            # process the data in form.cleaned_data as required
-            form.save()
-            user = User.objects.get(username=username)
-            user.set_password(password)
-            user = authenticate(username=username, password=password)
-            # A backend authenticated the credentials
-            if user is not None:
-                django_login(request, user)
-                username = form.cleaned_data["username"]
-                password = form.cleaned_data["password"]
-            # No backend authenticated the credentials
-            else:
-                print("")
-            # redirect to a new URL:
-            return HttpResponseRedirect("/home/")
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = SignupForm()
-    return render(request, "core/login.html", {"form": form})
-
-
 # --------- <home.html> ---------
 @login_required
 def home(request):
@@ -98,14 +68,3 @@ def profile(request):
 
 def test(request):
     return render(request, "core/test.html")
-
-@login_required
-def avatar(request):
-    if request.method == "POST":
-        form = UserProfileForm(request.POST)
-        if form.is_valid:
-            return HttpResponseRedirect("/home/")
-    else:
-        form = UserProfileForm()
-    return render(request, "core/profile.html", {"form": form})
-
