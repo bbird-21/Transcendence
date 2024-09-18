d8b89d3a8f4f6f7d75ffa6b620b98b4b7d377aac
mmeguedm <m.bbird21@proton.me>
2024-09-18 20:21:27 +0200

    [LOG-83 Add Friend] (#14)
    * add tty: True to have sync logging files with print()
    
    * add searchForm
    
    * addFriend done. acceptFriend done

--- django/ft_transcendence/core/models.py
+++ django/ft_transcendence/core/models.py
@@ -4,12 +4,18 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import User
 
+
+# Tips :
+#  -Django can't have two reverse query names that are the same \
+#   To prevent this behavior we use <related_name> to have unique "reverse query name" \
 class UserProfile(models.Model):
-    user   = models.OneToOneField(User, on_delete=models.CASCADE)
+    user   = models.OneToOneField(User, on_delete=models.CASCADE) # Here
     avatar = models.ImageField(upload_to="avatars/", default="avatar.png")
     victory = models.IntegerField(default=0)
     defeat = models.IntegerField(default=0)
+    friends = models.ManyToManyField(User, blank=True, related_name="userprofile_friends") # And here
     list_display = ['user', 'avatar']  # Customize fields to display
 
 @receiver(post_save, sender=User)
@@ -20,3 +26,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, related_name="from_user", on_delete=models.CASCADE)
+    to_user   = models.ForeignKey(User, related_name= "to_user", on_delete=models.CASCADE)
