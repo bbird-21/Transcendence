46ea4ccad333c8e1f2f8bdc9a86abcee01b5dd51
mmeguedm <m.bbird21@proton.me>
2024-10-02 14:51:35 +0200

    Chat in Progress (#21)
    * move css to dedicated file : social.css
    
    * fix(def-delete_pending_friend_request): Replace the OR operator with a AND operator to delete the appropriate FriendRequest
    
    * feat(/social/): It's now possible to remove a friend request that have been sent
    
    * feat(<user_profile>): Add Send Direct Message Buttton
    
    Also check if the user_profile is a friend before displaying Friend Request Button
    
    * feat(chat_app): Chat between two users in progress

--- django/ft_transcendence/chat/urls.py
+++ django/ft_transcendence/chat/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
+    path("send_direct_message/<int:userID>/", views.send_direct_message, name='send_direct_message')
 ]
