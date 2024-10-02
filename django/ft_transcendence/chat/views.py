400beba7908e7af76ab65b2e939786083f702fd5
MEGUEDMINI Mohamed <m.bbird21@proton.me>
2024-10-02 14:48:38 +0200

    feat(chat_app): Chat between two users in progress

--- django/ft_transcendence/chat/views.py
+++ django/ft_transcendence/chat/views.py
@@ -9,3 +9,9 @@ def index(request):
 @login_required
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
+
+# ---- Direct Message -------------------
+@login_required
+def send_direct_message(request, userID):
+    return render(request, "chat/direct_message.html")
+
