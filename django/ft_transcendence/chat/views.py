d85709312667d3e351c80b68d67a58b30bb32924
MEGUEDMINI Mohamed <m.bbird21@proton.me>
2024-11-02 17:11:41 +0100

    feat : add get_room_redirect > purpose : get room_name and redirect to room view

--- django/ft_transcendence/chat/views.py
+++ django/ft_transcendence/chat/views.py
@@ -6,29 +6,32 @@ from django.contrib.auth.models import User
 import json
 from django.utils.safestring import mark_safe
 from core.utils.profile_utils import get_or_create_chat
+from django.shortcuts import redirect
 
 @login_required
 def index(request):
     return render(request, "chat/index.html")
 
 @login_required
-def room(request, userID):
+def room(request, room_name, userID):
     from_user = request.user.id
     to_user   = User.objects.get(id=userID)
     user_profile = User.objects.get(id=userID)
-    chat = get_or_create_chat(request, user_profile)
-    room_name = chat.id
-
-    my_string = 'Hello'
 
     context = {
-        "my_string": my_string,
         "room_name": room_name,
         "userID": to_user.id,
         "sender_username": request.user.username,
         "receiver_username": to_user.username
     }
     return render(request, "chat/room.html", context)
+
+def get_room_redirect(request, userID):
+    user_profile = User.objects.get(id=userID)
+    chat = get_or_create_chat(request, user_profile)
+    room_name = chat.id
+
+    return redirect('chat:room', room_name=room_name, userID=userID)
 
 # ---- Direct Message -------------------
 @login_required
