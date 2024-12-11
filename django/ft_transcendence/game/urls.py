a1e96fb1a8053c3b8929887ad552c33a614f6d1e
mmeguedm <m.bbird21@proton.me>
2024-12-11 15:08:36 +0100

    [GT-15] Add Local Pong Game (#60)
    * redirect users to play page when all users are ready.
    
    * add countdown
    
    * feat(Game): add Local Pong Game

--- django/ft_transcendence/game/urls.py
+++ django/ft_transcendence/game/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'game'
 
 urlpatterns = [
-	path("play/", views.play, name='play'),
+	path("play/<int:waitingGameID>", views.play, name='play'),
 	path("waiting_game/<int:game_invitationID>", views.waiting_game, name='waiting_game'),
 	path("invitation/<int:userID>", views.game_invitation, name='game_invitation'),
 	path("accept_game/<int:game_invitationID>", views.accept_game, name="accept_game"),
